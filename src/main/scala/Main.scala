c51648b675bd48a5790e236b9ce2af66fe60c1c6
Maziyar PANAHI <maziyar.panahi@iscpif.fr>
2017-09-09 20:26:27 +0200

    Moving functions to Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,28 @@
+
 object Main {
   def main(args: Array[String]) {
     val env =args(0)
     val spark = SessionBuilder.buildSession(env)
-    PiplelineBuilder.definePipeline(spark, env)
+
+    val (dataDFRaw, predictDFRaw) = DataBuilder.loadData(env, spark, "train.csv", "test.csv")
+
+    val (dataDFExtra, predictDFExtra) = DataBuilder.createExtraFeatures(dataDFRaw, predictDFRaw)
+
+    val (dataDFCompleted, predictDFCompleted) = DataBuilder.fillNAValues(dataDFExtra, predictDFExtra)
+
+    val (labelIndexer, assembler, labelConverter, stringIndexers, dataDFFiltered, predictDFFiltered, featColName) =
+      FeatureBuilder.createVectors(
+        dataDFCompleted,
+        predictDFCompleted
+      )
+
+    val (cvModel, paramGrid) = PiplelineBuilder.fitPipeline(
+      spark, env,
+      labelIndexer, assembler, labelConverter, stringIndexers, featColName,
+      dataDFFiltered, predictDFFiltered
+    )
+
+    Evaluator.displayAllModels(cvModel, paramGrid)
 
   }
 }
